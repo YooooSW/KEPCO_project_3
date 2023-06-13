a1b2b639ea6b90aeb6a2ac62e87bb87bd845fabc
ThatHomr <34852381a@gmail.com>
2023-06-13 11:43:17 +0900

    23-06-13

--- Web_Django/mainapp/urls.py
+++ Web_Django/mainapp/urls.py
@@ -1,10 +1,25 @@
 from django.urls import path
+from django.urls import include
+from django.urls import re_path as url
+# from auth.apis import (
+#     LoginApi, 
+#     LogoutApi, 
+#     GoogleLoginApi, 
+#     GoogleSigninCallBackApi
+# )
+# from auth.googleapi import *
 
 ### from 뒤에 작성규칙
 #  - 폴더 경로 또는 폴더 경로 + 파일명
 ###
 
 from . import views
+
+# login_patterns = [
+#     path('google', GoogleLoginApi.as_view(), name='google_login'),
+#     path('google/callback', GoogleSigninCallBackApi.as_view(), name='google_login_callback'),
+# ]
+
 
 urlpatterns = [
 
@@ -23,6 +38,10 @@ urlpatterns = [
     # sns로그인 폼 연결
     ### http://127.0.0.1:8000/sns_login_form
     path('sns_login_form/', views.slogin_form),
+    ### http://127.0.0.1:8000/sns_login_form
+    path('sns_login_form/account/', views.slogin_form),
+    # path('logout', LogoutApi.as_view(), name="logout"),
+    # path('login/', include(login_patterns)),
 
     ### http://127.0.0.1:8000/login_form
     path('login_form/', views.login_form),
@@ -33,3 +52,4 @@ urlpatterns = [
     ### http://127.0.0.1:8000/search_pwd
     path('search_pwd/', views.search_pwd),
 ]
+
