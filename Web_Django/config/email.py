7f04217fe46fc9ec9ba7be7c80baec42efab46d8
ThatHomr <34852381a@gmail.com>
2023-06-22 12:40:34 +0900

    23-06-22 이메일 정보 삭제

--- Web_Django/config/email.py
+++ Web_Django/config/email.py
@@ -1,10 +1,10 @@
 class Email_address :
     
     def Email_get() :
-        EMAIL_HOST_USER = 'thathomr.dev@google.com' # 구글 이메일
+        EMAIL_HOST_USER = '' # 구글 이메일
         return EMAIL_HOST_USER
     
     def Email_password_get() :
-        EMAIL_HOST_PASSWORD = 'pbysghruudaxhiiw' # 2차 앱 키
+        EMAIL_HOST_PASSWORD = '' # 2차 앱 키
         return EMAIL_HOST_PASSWORD
     
